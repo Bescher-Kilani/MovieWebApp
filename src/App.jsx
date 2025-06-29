d3731d10fb5bce2b6e7c4e81521c7ebbe0477144
bescherkilani@gmail.com <bescherkilani@gmail.com>
2025-06-29 19:49:48 +0200

    add welcome bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>App</div>
+      <main>
+          <div className="pattern"></div>
+
+          <div className="wrapper"></div>
+          <header>
+              <h1>Find <span className="text-gradient"> Movies </span>
+                  You'll Enjoy with out the Hassle  </h1>
+          </header>
+
+      </main>
   );
 };
-## new repo
+
 export default App;
