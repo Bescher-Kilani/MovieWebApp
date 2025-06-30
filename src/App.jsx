6411247a4c2bdcafdf539613b8dfb090101241ca
bescherkilani@gmail.com <bescherkilani@gmail.com>
2025-06-30 21:05:45 +0200

    added figma designs

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ const App = () => {
       <main>
           <div className="pattern"></div>
 
-          <div className="wrapper"></div>
+          <div className="wrapper">
           <header>
+               <img src="./hero.png" alt="Hero Banner" />
               <h1>Find <span className="text-gradient"> Movies </span>
                   You'll Enjoy with out the Hassle  </h1>
           </header>
-
+      </div>
       </main>
   );
 };
