a8c0df19df73cd145e8a2bf3830c86dcd1ec3e90
bescherkilani@gmail.com <bescherkilani@gmail.com>
2025-07-02 20:00:25 +0200

    added searchbar and API_KEY

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Search from "./components/Search.jsx";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+    }
+}
+
+
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+
+
+    },);
+
   return (
       <main>
           <div className="pattern"></div>
@@ -11,6 +32,10 @@ const App = () => {
               <h1>Find <span className="text-gradient"> Movies </span>
                   You'll Enjoy with out the Hassle  </h1>
           </header>
+
+           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+              <h1 className="text-white">{searchTerm}</h1>
       </div>
       </main>
   );
