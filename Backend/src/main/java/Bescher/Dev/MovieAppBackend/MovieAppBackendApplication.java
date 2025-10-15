ba27533eb6e2d2924df928c4b135b75dea46e81d
bescherkilani@gmail.com <bescherkilani@gmail.com>
2025-10-15 17:11:36 +0200

    add PostgreSQL Docker Container

--- Backend/src/main/java/Bescher/Dev/MovieAppBackend/MovieAppBackendApplication.java
+++ Backend/src/main/java/Bescher/Dev/MovieAppBackend/MovieAppBackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.MovieAppBackend;
+package Bescher.Dev.MovieAppBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
